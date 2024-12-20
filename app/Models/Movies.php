820c0196b67dd810043bcdc6b81fc64d850f972c
mjaya69703 <mjaya69703@gmail.com>
2024-12-21 01:11:35 +0700

    first commit

---
+++ app/Models/Movies.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Movies extends Model
+{
+
+    protected $guarded = [];
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
+    }
+}
